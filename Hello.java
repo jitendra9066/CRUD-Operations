c00c72e371ae2983eb6e2f43bf7edb44937ee3f1
jitendra9066 <86509442+jitendra9066@users.noreply.github.com>
2022-03-17 16:54:46 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+import com.jpa.test.entities.user;
+public class bootjpaApplication
+{
+public static void main(String[]args)
+{
+ApplicationContext context=new SpringApplication.run(BootjpaexampleApplication.class,args);
+UserRepository userRepository=context.getbean(UserRepository.class);
+//Create object of user
+User user1=new User();
+user1.setName=("Uttam");
+user1.setcity=("city1");
+user1.setstatus=("python");
+User user2=new User();
+user2.getName="ankit";
+user2.getcity="city2";
+user2.getstatus="java";
+UserResult user=UserRepository.save(user);
+System.out.println("save user"+resultUser);
+System.out.println("done");
+}
+}
